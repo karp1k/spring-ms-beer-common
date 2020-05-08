a3fdee7b2ca86046e32d3d8dd505d9d1679245a2
Tom <desktop-vm-ubuntu>
2020-05-08 13:49:02 +0300

    Add Constants

---
+++ src/main/java/guru/springframework/springmsbeercommon/web/config/Constants.java
@@ -0,0 +1,9 @@
+package guru.springframework.springmsbeercommon.web.config;
+
+/**
+ * @author kas
+ */
+public final class Constants {
+
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+}
