3913665983f18fbaeaf8130ce84de8786c9ba70c
Tom <desktop-vm-ubuntu>
2020-05-08 11:12:01 +0300

    Init project moved shared dto from services to external project

---
+++ src/main/java/guru/springframework/springmsbeercommon/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.springmsbeercommon.web.model;
+
+/**
+ * @author kas
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STAUT, IPA, GOSE, PORTER, ALE, WHEAT, PALE_ALE
+}
