47b52e091ec243c6aff338274a3670d3c3b7335a
Tom <desktop-vm-ubuntu>
2020-05-18 14:07:01 +0300

    Add queue for beer order allocation Set project encoding

--- src/main/java/guru/springframework/springmsbeercommon/web/config/Constants.java
+++ src/main/java/guru/springframework/springmsbeercommon/web/config/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
     public static final String VALIDATE_ORDER_QUEUE = "validate-order";
     public static final String VALIDATE_ORDER_RESULT_QUEUE = "validate-order-result";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
 }
